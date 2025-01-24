e6036ab71aeed3358146fc7ae2ed2597d25604f7
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-24 15:23:41 +0200

    Add reef buttons

--- lib/widgets/driver/reef_selector.dart
+++ lib/widgets/driver/reef_selector.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dashboard24/services/dashboard_state.dart';
 import 'package:flutter/material.dart';
 
@@ -27,86 +29,55 @@ class _ReefSelectorState extends State<ReefSelector> {
   Widget build(BuildContext context) {
     Color activeColor = widget.redAlliance ? Colors.red[700]! : Colors.indigo;
 
-    return SizedBox(
+    return Transform.scale(
+      scale: 1.5,
+      child: SizedBox(
       width: 450,
       height: 450,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Stack(
           children: [
-            widget.redAlliance ? Image.asset('images/reef_red.png') : Image.asset('images/reef_blue.png'),
-            Positioned(
-              left: 90,
-              top: 220,
-              child: Transform.scale(
-                scale: 3.0,
-                child: Checkbox(
-                  value: _selected == 0,
-                  splashRadius: 9,
-                  checkColor: Colors.white,
-                  activeColor: activeColor,
-                  shape: const CircleBorder(),
-                  side: const BorderSide(width: 0.5, color: Colors.grey),
-                  onChanged: (value) {
-                    setState(() {
-                      if (value ?? false) {
-                        _selected = 0;
-                        widget.dashboardState.setReefPose(_selected);
-                      }
-                    });
-                  },
+            widget.redAlliance
+                ? Image.asset('images/reef_red.png', fit: BoxFit.fill)
+                : Image.asset('images/reef_blue.png', fit: BoxFit.fill),
+            ...List.generate(12, (index) {
+              double angle = ((index * 30) + 15) * (3.141592653589793 / 180);
+              double radius = 150;
+
+              double left = 450 / 2 + radius * cos(angle) - 20;
+              double top = 450 / 2 + radius * sin(angle) - 20;
+
+              return Positioned(
+                left: left,
+                top: top,
+                child: Transform.rotate(
+                  angle: 0.0,
+                  child: Transform.scale(
+                    scale: 3.0,
+                    child: Checkbox(
+                      value: _selected == index,
+                      splashRadius: 9,
+                      checkColor: Colors.white,
+                      activeColor: activeColor,
+                      shape: const CircleBorder(),
+                      side: const BorderSide(width: 0.5, color: Colors.grey),
+                      onChanged: (value) {
+                        setState(() {
+                          if (value ?? false) {
+                            _selected = index;
+                            widget.dashboardState.setReefPose(_selected);
+                          }
+                        });
+                      },
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Positioned(
-              left: 200,
-              top: 35,
-              child: Transform.scale(
-                scale: 3.0,
-                child: Checkbox(
-                  value: _selected == 1,
-                  splashRadius: 9,
-                  checkColor: Colors.white,
-                  activeColor: activeColor,
-                  shape: const CircleBorder(),
-                  side: const BorderSide(width: 0.5, color: Colors.grey),
-                  onChanged: (value) {
-                    setState(() {
-                      if (value ?? false) {
-                        _selected = 1;
-                        widget.dashboardState.setReefPose(_selected);
-                      }
-                    });
-                  },
-                ),
-              ),
-            ),
-            Positioned(
-              left: 315,
-              top: 220,
-              child: Transform.scale(
-                scale: 3.0,
-                child: Checkbox(
-                  value: _selected == 2,
-                  splashRadius: 9,
-                  checkColor: Colors.white,
-                  activeColor: activeColor,
-                  shape: const CircleBorder(),
-                  side: const BorderSide(width: 0.5, color: Colors.grey),
-                  onChanged: (value) {
-                    setState(() {
-                      if (value ?? false) {
-                        _selected = 2;
-                        widget.dashboardState.setReefPose(_selected);
-                      }
-                    });
-                  },
-                ),
-              ),
-            ),
+              );
+            }),
           ],
         ),
       ),
-    );
+    ));
   }
 }
